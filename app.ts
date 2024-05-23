8a91b438a175be2f6abbf04b5c0071fc15ab0bde
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-05-23 00:30:56 -0400

    fix: updated typescript types in app.ts

--- app.ts
+++ app.ts
@@ -2,21 +2,24 @@ import express from 'express';
 import logger from 'morgan';
 import createError from "http-errors";
 import dotenv from 'dotenv';
+import {Request,Response,NextFunction} from 'express';
+import routes from './routes';
+
 dotenv.config();
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
-app.get('/', (req, res) => {
+app.use('/api/users',routes.users);
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello, Express!');
 });
-app.use((req, res, next) => {
+app.use((req:Request, res:Response, next) => {
     next(createError(404));
 });
   
 
-app.use((err, req, res, next) => {
+app.use((err:any, req:Request, res:Response, next:NextFunction) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500).json({ error: err.message });
