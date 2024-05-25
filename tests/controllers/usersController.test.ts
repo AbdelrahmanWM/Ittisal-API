7b090852a168ae91f39760d82ac1712ca7ccd074
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-05-25 00:22:27 -0400

    fix(test) update test to connect to the db

--- tests/controllers/usersController.test.ts
+++ tests/controllers/usersController.test.ts
@@ -1,24 +1,36 @@
 import request from "supertest";
 import app from "../../app";
 import routes from "../../routes";
+import mongoose from "mongoose";
 
 import { IUser } from "../../interfaces/IUser";
 
 
-test("Get all users", async () => {
-  const response = await request(app)
-    .get("/api/users")
-    .expect("Content-Type", /json/)
-    .expect(200);
-  expect(Array.isArray(response.body)).toBe(true);
-  response.body.forEach((user: IUser) => {
-    expect(user).toHaveProperty("firstName");
-    expect(user).toHaveProperty("lastName");
-    expect(user).toHaveProperty('username');
-    expect(user).toHaveProperty('email');
-    expect(user).toHaveProperty('password');
-    expect(user).toHaveProperty('role');
-
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGO_URI||"");
   });
-});
+  
+
+afterEach(async () => {
+    await mongoose.connection.close();
+  });
+  
+test("Get all users", async () => {
+    const response = await request(app)
+      .get("/api/users")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  
+    expect(Array.isArray(response.body.data)).toBe(true);
+    const users: IUser[] = response.body.data;
+    users.forEach((user: IUser) => {
+      expect(user).toHaveProperty("firstName");
+      expect(user).toHaveProperty("lastName");
+      expect(user).toHaveProperty("username");
+      expect(user).toHaveProperty("email");
+      expect(user).toHaveProperty("password");
+      expect(user).toHaveProperty("role");
+    
+    });
+  });
 
