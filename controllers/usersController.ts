08d2d8b2ccfe81e68492a3141a6f6007a798cbaa
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-05-26 21:44:04 -0400

    fix(users) return only 'user' type users in user getters

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -5,7 +5,7 @@ import { sendErrorResponse, sendSuccessResponse } from '../utilities/Response';
 
 export const user_list = async (req: Request, res: Response): Promise<void> => {
     try {
-        const users: IUser[] = await User.find({}).sort({ username: 1 });
+        const users: IUser[] = await User.find({role:"user"}).sort({ username: 1 });
         if (users.length === 0) {
             return sendErrorResponse(res, "No users found.", { code: "NO_USERS_FOUND" }, 404);
         }
@@ -20,9 +20,10 @@ export const user_detail = async (req: Request, res: Response): Promise<void> =>
     const {userId} = req.params;
     try {
         const user: IUser | null = await User.findById(userId);
-        if (!user)  {
+        if (!user || user.role!='user')  {
             return sendErrorResponse(res, "User not found.", { code: "NO_USER_FOUND" }, 404);
         }
+
         sendSuccessResponse(res, "Successfully fetched user details.", user, 200);
     } catch (error) {
 
